17438ae131a4624f8227e5b47afb7beb55a1b720
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2017-12-09 11:21:53 -0200

    Enhanced held_map iterators.

--- held_map.h
+++ held_map.h
@@ -17,8 +17,10 @@ public:
 	struct entry final {
 		keyT   key;
 		valueT value;
+
 		entry() = default;
 		explicit entry(const keyT& key) noexcept : key(key) { }
+		entry(const keyT& key, const valueT& value) noexcept : key(key), value(value) { }
 	};
 
 private:
@@ -26,12 +28,19 @@ private:
 
 public:
 	held_map() = default;
+	held_map(held_map&& other) noexcept { this->operator=(std::move(other)); }
 	held_map(std::initializer_list<entry> entries) noexcept : _entries{entries} { }
 
 	size_t    size() const noexcept               { return this->_entries.size(); }
 	bool      empty() const noexcept              { return this->_entries.empty(); }
 	held_map& clear() noexcept                    { this->_entries.clear(); return *this; }
 	held_map& reserve(size_t numEntries) noexcept { this->_entries.reserve(numEntries); return *this; }
+
+	held_map& operator=(held_map&& other) noexcept {
+		this->empty();
+		this->_entries.swap(other._entries);
+		return *this;
+	}
 
 	const valueT& operator[](const keyT& key) const {
 		std::vector<entry>::const_iterator ite = this->_find(key);
@@ -102,26 +111,50 @@ public:
 	private:
 		typename std::vector<entry>::const_iterator _it;
 	public:
+		const_iterator() = default;
+		const_iterator(const const_iterator& other) noexcept { this->operator=(other); }
 		const_iterator(typename std::vector<entry>::const_iterator it) noexcept : _it(it) { }
-		const_iterator operator++() noexcept                                  { const_iterator tmp = *this; this->_it++; return tmp; }
-		const_iterator operator++(int) noexcept                               { this->_it++; return *this; }
-		const entry&   operator*() noexcept                                   { return this->_it.operator*(); }
-		const entry*   operator->() noexcept                                  { return this->_it.operator->(); }
-		bool           operator==(const const_iterator& other) const noexcept { return this->_it == other._it; }
-		bool           operator!=(const const_iterator& other) const noexcept { return !this->operator==(other); }
+
+		const_iterator& operator=(const const_iterator& other) noexcept { this->_it = other._it; return *this; }
+		const_iterator  operator+(std::ptrdiff_t off) const { return {this->_it + off}; }
+		const_iterator  operator-(std::ptrdiff_t off) const { return {this->_it - off}; }
+		const_iterator& operator+=(std::ptrdiff_t off)      { this->_it += off; return *this; }
+		const_iterator& operator-=(std::ptrdiff_t off)      { this->_it -= off; return *this; }
+		const_iterator& operator++()       { ++this->_it; return *this; }
+		const_iterator  operator++(int)    { const_iterator tmp = *this; ++this->_it; return tmp; }
+		const_iterator& operator--()       { --this->_it; return *this; }
+		const_iterator  operator--(int)    { const_iterator tmp = *this; --this->_it; return tmp; }
+		const entry&    operator*() const  { return this->_it.operator*(); }
+		const entry*    operator->() const { return this->_it.operator->(); }
+		bool            operator==(const const_iterator& other) const noexcept { return this->_it == other._it; }
+		bool            operator!=(const const_iterator& other) const noexcept { return !this->operator==(other); }
+		bool            operator>(const const_iterator& other) const noexcept  { return this->_it > other._it; }
+		bool            operator<(const const_iterator& other) const noexcept  { return this->_it < other._it; }
 	};
 
 	class iterator final {
 	private:
 		typename std::vector<entry>::iterator _it;
 	public:
+		iterator() = default;
+		iterator(const iterator& other) noexcept { this->operator=(other); }
 		iterator(typename std::vector<entry>::iterator it) noexcept : _it(it) { }
-		iterator operator++() noexcept                            { iterator tmp = *this; this->_it++; return tmp; }
-		iterator operator++(int) noexcept                         { this->_it++; return *this; }
-		entry&   operator*() noexcept                             { return this->_it.operator*(); }
-		entry*   operator->() noexcept                            { return this->_it.operator->(); }
-		bool     operator==(const iterator& other) const noexcept { return this->_it == other._it; }
-		bool     operator!=(const iterator& other) const noexcept { return !this->operator==(other); }
+
+		iterator& operator=(const iterator& other) noexcept { this->_it = other._it; return *this; }
+		iterator  operator+(std::ptrdiff_t off) const { return {this->_it + off}; }
+		iterator  operator-(std::ptrdiff_t off) const { return {this->_it - off}; }
+		iterator& operator+=(std::ptrdiff_t off)      { this->_it += off; return *this; }
+		iterator& operator-=(std::ptrdiff_t off)      { this->_it -= off; return *this; }
+		iterator& operator++()    { ++this->_it; return *this; }
+		iterator  operator++(int) { iterator tmp = *this; ++this->_it; return tmp; }
+		iterator& operator--()    { --this->_it; return *this; }
+		iterator  operator--(int) { iterator tmp = *this; --this->_it; return tmp; }
+		entry&    operator*()     { return this->_it.operator*(); }
+		entry*    operator->()    { return this->_it.operator->(); }
+		bool      operator==(const iterator& other) const noexcept { return this->_it == other._it; }
+		bool      operator!=(const iterator& other) const noexcept { return !this->operator==(other); }
+		bool      operator>(const iterator& other) const noexcept  { return this->_it > other._it; }
+		bool      operator<(const iterator& other) const noexcept  { return this->_it < other._it; }
 	};
 
 	const_iterator cbegin() const noexcept { return {this->_entries.cbegin()}; }
