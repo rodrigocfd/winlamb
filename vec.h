042cc49bf48b0e51a185bfb5558a8728ab4e18e1
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2018-06-03 14:22:41 -0300

    Adding vector utilities.

---
+++ vec.h
@@ -0,0 +1,67 @@
+/**
+ * Part of WinLamb - Win32 API Lambda Library
+ * https://github.com/rodrigocfd/winlamb
+ * Copyright 2017-present Rodrigo Cesar de Freitas Dias
+ * This library is released under the MIT License
+ */
+
+#pragma once
+#include <algorithm>
+#include <vector>
+
+namespace wl {
+
+// Utilities to std::vector.
+class vec final {
+private:
+	vec() = delete;
+
+public:
+	template<typename T>
+	static size_t find(const std::vector<T>& v, const T& value) {
+		std::vector<T>::const_iterator it = std::find(v.cbegin(), v.cend(), value);
+		return it == v.cend() ? -1 : it - v.cbegin();
+	}
+
+	template<typename T, typename predicateT>
+	static size_t find_if(const std::vector<T>& v, predicateT&& func) {
+		std::vector<T>::const_iterator it = std::find_if(v.cbegin(), v.cend(), std::move(func));
+		return it == v.cend() ? -1 : it - v.cbegin();
+	}
+
+	template<typename T>
+	static bool exists(const std::vector<T>& v, const T& value) {
+		return find(v, value) != -1;
+	}
+
+	template<typename T, typename predicateT>
+	static bool exists_if(const std::vector<T>& v, predicateT&& func) {
+		return find_if(v, std::move(func)) != -1;
+	}
+
+	template<typename T>
+	static void append(std::vector<T>& v, const std::vector<T>& other) {
+		v.insert(v.end(), other.cbegin(), other.cend());
+	}
+
+	template<typename T>
+	static void remove(std::vector<T>& v, size_t index) {
+		v.erase(v.begin() + index);
+	}
+
+	template<typename T, typename predicateT>
+	static void remove_if(std::vector<T>& v, predicateT&& func) {
+		// https://stackoverflow.com/a/9053941/6923555
+		// Usage example:
+		// vector<wstring> de = {L"a", L"b", L"c"};
+		// vec::remove_if(de, [](const wstring& d)->bool {
+		//		return d == L"a";
+		// });
+		v.erase(
+			std::remove_if(v.begin(), v.end(), std::move(func)),
+			v.end()
+		);
+	}
+};
+
+}//namespace wl
