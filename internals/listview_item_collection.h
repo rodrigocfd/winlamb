05f4a77f49cbe12defe0d1ecd15fd2b1ee313172
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2019-09-29 22:25:06 -0300

    Documenting listview::item_collection.

--- internals/listview_item_collection.h
+++ internals/listview_item_collection.h
@@ -27,6 +27,7 @@ public:
 		return {itemIndex, this->_hList};
 	}
 
+	// Returns the number of items in the listview.
 	size_t count() const noexcept {
 		return ListView_GetItemCount(this->_hList);
 	}
@@ -83,47 +84,54 @@ public:
 		return this->add_at_pos_with_icon(caption, -1, -1);
 	}
 
+	// Returns a vector with all items in the listview.
 	std::vector<listview_item> get_all() const {
 		size_t totItems = this->count();
-		std::vector<listview_item> items; // a big array with all items in list
+		std::vector<listview_item> items;
 		items.reserve(totItems);
 		for (size_t i = 0; i < totItems; ++i) {
-			items.emplace_back(i, this->_hList);
+			items.emplace_back(i, this->_hList); // construct a listview_item object for each item
 		}
 		return items;
 	}
 
+	// Removes all items from the listview.
 	listview_item_collection& remove_all() noexcept {
 		ListView_DeleteAllItems(this->_hList);
 		return *this;
 	}
 
+	// Returns the first item which caption matches the given string, case-insensitive, or listview_item::npos if none is found.
 	listview_item find(const wchar_t* caption) const noexcept {
 		LVFINDINFOW lfi{};
 		lfi.flags = LVFI_STRING; // search is case-insensitive
 		lfi.psz = caption;
 		return {static_cast<size_t>(ListView_FindItem(this->_hList, -1, &lfi)),
-			this->_hList}; // returns listview_item::npos if not found
+			this->_hList};
 	}
 
+	// Returns the first item which caption matches the given string, case-insensitive, or listview_item::npos if none is found.
 	listview_item find(const std::wstring& caption) const noexcept {
 		return this->find(caption.c_str());
 	}
 
+	// Returns true if there is an item which caption matches the given string, case-insensitive.
 	bool exists(const wchar_t* caption) const noexcept {
 		return this->find(caption).index != listview_item::npos;
 	}
 
+	// Returns true if there is an item which caption matches the given string, case-insensitive.
 	bool exists(const std::wstring& caption) const noexcept {
 		return this->exists(caption.c_str());
 	}
 
+	// Returns the number of selected items in the listview.
 	size_t count_selected() const noexcept {
 		return ListView_GetSelectedCount(this->_hList);
 	}
 
+	// Selects the items whose indexes have been passed in the array.
 	listview_item_collection& select(const std::vector<size_t>& indexes) noexcept {
-		// Select the items whose indexes have been passed in the array.
 		for (const size_t index : indexes) {
 			ListView_SetItemState(this->_hList,
 				static_cast<int>(index), LVIS_SELECTED, LVIS_SELECTED);
@@ -131,16 +139,19 @@ public:
 		return *this;
 	}
 
+	// Selects all items in the listview.
 	listview_item_collection& select_all() noexcept {
 		ListView_SetItemState(this->_hList, -1, LVIS_SELECTED, LVIS_SELECTED);
 		return *this;
 	}
 
+	// Unselects all items in the listview.
 	listview_item_collection& select_none() noexcept {
 		ListView_SetItemState(this->_hList, -1, 0, LVIS_SELECTED);
 		return *this;
 	}
 
+	// Removes all selected items in the listview.
 	listview_item_collection& remove_selected() noexcept {
 		SendMessageW(this->_hList, WM_SETREDRAW, static_cast<WPARAM>(FALSE), 0);
 		int i = -1;
@@ -151,15 +162,18 @@ public:
 		return *this;
 	}
 
+	// Returns the last item in the listview.
 	listview_item get_last() const noexcept {
 		return this->operator[](this->count() - 1);
 	}
 
+	// Returns the topmost visible item when in list or report view.
 	listview_item get_first_visible() const noexcept {
 		return {static_cast<size_t>(ListView_GetTopIndex(this->_hList)),
 			this->_hList};
 	}
 
+	// Returns all selected items in the listview.
 	std::vector<listview_item> get_selected() const {
 		std::vector<listview_item> items;
 		items.reserve(this->count_selected());
@@ -173,6 +187,7 @@ public:
 		return items;
 	}
 
+	// Returns the indexes of all selected items in the listview.
 	std::vector<size_t> get_selected_indexes() const {
 		std::vector<size_t> indexes;
 		indexes.reserve(this->count_selected());
@@ -186,16 +201,19 @@ public:
 		return indexes;
 	}
 
+	// Returns the focused item in the listview, or listview_item::npos if none.
 	listview_item get_focused() const noexcept {
 		return {static_cast<size_t>(ListView_GetNextItem(this->_hList, -1, LVNI_FOCUSED)),
 			this->_hList};
 	}
 
+	// Returns the index of the focused item in the listview.
 	size_t get_focused_index() const noexcept {
 		int iFoc = ListView_GetNextItem(this->_hList, -1, LVNI_FOCUSED);
 		return (iFoc == -1) ? listview_item::npos : static_cast<size_t>(iFoc);
 	}
 
+	// Return the texts, from the given column, from the given items.
 	std::vector<std::wstring> get_texts(const std::vector<listview_item>& itemsToGet,
 		size_t columnIndex = 0) const
 	{
